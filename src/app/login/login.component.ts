ec0c553fe64868623875877aa8b9c41f1e70206d
unknown <aparnasomzz@gmail.com>
2023-03-06 12:41:19 +0530

    third commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   data="Perfect banking Partner"
   d2="Enter Ac Number"
-  // acno:any
-  // psword:any
+  acno:any
+  psword:any
   userDetails:any = {
     1000: { username: "anu", acno: 1000, password: "abc123", balance: 0 },
     1001: { username: "manu", acno: 1001, password: "def123", balance: 0 }, 
@@ -17,22 +17,22 @@ export class LoginComponent {
     1003: { username: "sanu", acno: 1003, password: "jkl123", balance:0 }
 
   }
-//   login(){
-//     var acnum=this.acno
-//     var psw=this.psword
-//     var userdata=this.userDetails
-//     // alert("login succes")
-//     if (acnum in userdata) {
-//       if (psw == userdata[acnum]["password"]) {
-//         alert("Login success")
-//       } else {
-//         alert("Incorrect Password!")
-//       }
-//     } else {
-//       alert('Incorrect Account number!')
-//     }
+  login(){
+    var acnum=this.acno
+    var psw=this.psword
+    var userdata=this.userDetails
+    // alert("login succes")
+    if (acnum in userdata) {
+      if (psw == userdata[acnum]["password"]) {
+        alert("Login success")
+      } else {
+        alert("Incorrect Password!")
+      }
+    } else {
+      alert('Incorrect Account number!')
+    }
 
-//   }
+  }
 //   // acnoChange(event:any){
 //   //   this.acno=event.target.value
     
@@ -42,20 +42,20 @@ export class LoginComponent {
     
 //   // }
 //template rendering variable method
-login(acno:any,pw:any){
-  var acnum=acno.value
-  var psw=pw.value
-  var userdata=this.userDetails
-  // alert("login succes")
-  if (acnum in userdata) {
-    if (psw == userdata[acnum]["password"]) {
-      alert("Login success")
-    } else {
-      alert("Incorrect Password!")
-    }
-  } else {
-    alert('Incorrect Account number!')
-  }
+// login(acno:any,pw:any){
+//   var acnum=acno.value
+//   var psw=pw.value
+//   var userdata=this.userDetails
+//   // alert("login succes")
+//   if (acnum in userdata) {
+//     if (psw == userdata[acnum]["password"]) {
+//       alert("Login success")
+//     } else {
+//       alert("Incorrect Password!")
+//     }
+//   } else {
+//     alert('Incorrect Account number!')
+//   }
 
-}
+// }
 }
